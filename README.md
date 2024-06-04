59f72362ec34058e53f9f16eb7a80fe6fae74aff
Mohamed Sharif <135287156+mSharifHub@users.noreply.github.com>
2024-06-04 16:02:06 -0700

    included register user test case and updated Readme doc

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
-# django_rest_api
+# Django Registration Endpoint Testing
 
-### 
+ ## Prerequisites
+
+- Python installed
+- Django installed
+- Django REST Framework installed
+## Setting Up the Project
+
+1. **Clone the repository**:
+   ```sh
+   git clone https://github.com/your-repo/django_back_end.git
+   cd django_back_end
+   
+2. **Create and activate env**:
+   ```sh
+   python -m venv venv
+   source venv/bin/activate 
+   
+3. **Install dependencies**:
+   ```sh
+    pip install -r requirements.txt
+
+
+
+ ## Testing User Registrations
+
+1. **Register User**:
+   ```sh
+   python manage.py test user_app
--- user_app/tests.py
+++ user_app/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
+from django.urls import reverse
+from rest_framework import status
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class RegisterTestCase(APITestCase):
+    def test_register_user(self):
+        url = reverse('register')
+        data = {
+            'username': 'mock-user',
+            'email': 'mock-user@mail.com',
+            'password': '12345678910',
+            'confirm_password': '12345678910'
+
+        }
+
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(User.objects.get().username, 'mock-user')
