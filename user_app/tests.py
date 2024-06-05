67f1015551150baf935b3097f7420631eaade5f8
Mohamed Sharif <135287156+mSharifHub@users.noreply.github.com>
2024-06-04 17:15:50 -0700

    testing for login and logout

--- user_app/tests.py
+++ user_app/tests.py
@@ -30,6 +30,59 @@ class RegisterTestCase(APITestCase):
             'confirm_password': '12345678910',
         }
         response = self.client.post(url, data, format='json')
-        print(response.json())
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
+
+class AuthenticateTestCase(APITestCase):
+    def setUp(self):
+        self.register_url = reverse('register')
+        self.login_url = reverse('login')
+        self.logout_url = reverse('logout')
+
+        self.user_data = {
+            'username': 'mock-user',
+            'email': 'mock-user@mail.com',
+            'password': '<PASSWORD>',
+            'confirm_password': '<PASSWORD>'
+        }
+
+        self.client.post(self.register_url, self.user_data, format='json')
+
+    def test_login_user(self):
+        data = {
+            'username': 'mock-user',
+            'password': '<PASSWORD>',
+        }
+        response = self.client.post(self.login_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('token', response.json())
+        self.assertIn('refresh', response.json())
+
+    def test_logout_user(self):
+        login_data = {
+            'username': 'mock-user',
+            'password': '<PASSWORD>',
+        }
+        login_response = self.client.post(self.login_url, login_data, format='json')
+        refresh_token = login_response.json()['refresh']
+
+        logout_data = {
+            'refresh': refresh_token,
+
+        }
+
+        logout_response = self.client.post(self.logout_url, logout_data, format='json')
+        self.assertEqual(logout_response.status_code, status.HTTP_200_OK)
+        self.assertIn("logged out successfully", logout_response.json()['message'])
+
+    def test_invalid_user(self):
+        data = {'username': 'mock-user', 'password': '<PASSD>'}
+        response = self.client.post(self.login_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_logout_invalid_token(self):
+        data = {
+            'refresh': "invalid token"
+        }
+        response = self.client.post(self.logout_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
